d81e301247cd648e65a8fefcfa74b284ad773264
realdiganta <47485188+dxganta@users.noreply.github.com>
2024-03-15 22:19:59 +0530

    sandclock logo redirect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,13 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-300 px-10 py-2">
       <div className="flex-1">
-        <img src="sandclock.png" alt="Sandclock" />
+        <a
+          href="https://app.sandclock.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="sandclock.png" alt="Sandclock" />
+        </a>
       </div>
       <div className="grow font-semibold text-2xl text-white">
         Quartz Airdrop Leaderboard
