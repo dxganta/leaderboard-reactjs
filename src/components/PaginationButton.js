bbc2052d3c9a73ba5123f1f689b04ee1e6fc5a92
realdiganta <47485188+dxganta@users.noreply.github.com>
2024-04-05 15:13:59 +0530

    pagination color fix

--- src/components/PaginationButton.js
+++ src/components/PaginationButton.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 const PaginationButton = ({ pageNumber, onPageChange, currentPage }) => {
+  if (pageNumber === currentPage) {
+    return (
+      <button
+        onClick={() => onPageChange(pageNumber)}
+        className="join-item btn text-base font-normal bg-neutral-700 text-white"
+      >
+        {pageNumber}
+      </button>
+    );
+  }
+
   return (
     <button
       onClick={() => onPageChange(pageNumber)}
-      className={`join-item btn bg-white hover:text-white text-neutral-800 text-base font-normal ${
-        pageNumber === currentPage && "bg-neutral-800 text-white"
-      }`}
+      className="join-item btn bg-white hover:text-white text-neutral-800 text-base font-normal"
     >
       {pageNumber}
     </button>
