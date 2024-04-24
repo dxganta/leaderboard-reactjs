6c4195d7e862c823e68f9c867777a1e360245623
realdiganta <47485188+dxganta@users.noreply.github.com>
2024-04-24 20:23:35 +0700

    faq accordion fix

--- src/components/FaqAccordion.js
+++ src/components/FaqAccordion.js
@@ -3,7 +3,7 @@ import React from "react";
 const FaqAccordion = ({ question, answer }) => {
   return (
     <div className="collapse collapse-arrow bg-neutral-800 my-3">
-      <input type="radio" name="my-accordion-2" />
+      <input type="checkbox" />
       <div className="collapse-title text-base font-normal">{question}</div>
       <div className="collapse-content">
         <p>{answer}</p>
